02c0713bf46da4f9738c57bff7afe975cc9d8651
kevingo <kevingo75@gmail.com>
2016-12-07 15:13:53 +0800

    Add config template

---
+++ config.go
@@ -0,0 +1,13 @@
+package main
+
+var (
+	QueueUrl       = ""
+	FromBucketName = ""
+	ToBucketName   = ""
+	Region         = ""
+	CNRegion       = ""
+	SESRegion      = ""
+	AccessKey      = ""
+	SecretKey      = ""
+	Email          = ""
+)
